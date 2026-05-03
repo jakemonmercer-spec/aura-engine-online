28bfc13611000e9e2f7765316d2c1ad65225b29d
jakemonmercer-spec <jakemonmercer@gmail.com>
2026-05-03 14:04:23 +0600

    Refactor Firebase config to exclude storage
    Removed storage initialization and related comments.

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,3 @@
-// firebase-config.js
-// Используем COMPAT-версию (без import, только глобальные переменные)
-
 const firebaseConfig = {
   apiKey: "AIzaSyAnwfcKCVB35c4ReExi3KK1fH2b3hPeJSE",
   authDomain: "auraengineonline.firebaseapp.com",
@@ -10,10 +7,6 @@ const firebaseConfig = {
   appId: "1:433967688454:web:1011b44105de083d68c87",
   measurementId: "G-4C30YKMBZR"
 };
-
-// Инициализация Firebase (глобальные объекты firebase, firebase.firestore)
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const storage = firebase.storage();
-
-console.log("✅ Firebase подключён, db и storage доступны");
+// Storage НЕ ИСПОЛЬЗУЕМ, только Firestore
