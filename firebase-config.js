cb141e0947cda9c02c9130d26ae7330d95a325f6
jakemonmercer-spec <jakemonmercer@gmail.com>
2026-05-03 14:59:18 +0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
+// firebase-config.js
 const firebaseConfig = {
   apiKey: "AIzaSyAnwfcKCVB35c4ReExi3KK1fH2b3hPeJSE",
   authDomain: "auraengineonline.firebaseapp.com",
   projectId: "auraengineonline",
   storageBucket: "auraengineonline.firebasestorage.app",
   messagingSenderId: "433967668454",
-  appId: "1:433967688454:web:1011b44105de083d68c87",
-  measurementId: "G-4C30YKMBZR"
+  appId: "1:433967688454:web:1011b44105de083d68c87"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-// Storage НЕ ИСПОЛЬЗУЕМ, только Firestore
+// Storage не используется – все файлы через GitHub API
